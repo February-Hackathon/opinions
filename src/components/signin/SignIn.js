c750d8480d681e0e8dbdb455a2f199fb21c05118
Ryan McLaughlin <itsryanmclaughlin@gmail.com>
2021-02-10 16:05:58 -0500

    sign in screen responsive

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
 import { Link, Redirect } from 'react-router-dom';
+import './signin.css';
+import signInImg from './signin_pic.png';
 
 const SignIn = ({ loggedIn, setLoggedIn }) => {
     const initialState = {
@@ -28,38 +30,45 @@ const SignIn = ({ loggedIn, setLoggedIn }) => {
 	}
 	return (
 		<div>
-			<div className='loginTitle'>Login Below:</div>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor='email' className='emailLabel'>
-					Enter Email:{' '}
-				</label>
-				<input
-					id='email'
-					onChange={handleChange}
-					value={formState.email}
-					placeholder='Email'
-					className='emailInput'
-				/>{' '}
+			<div className='body'>
+				<div className='signinTitle'>Sign In</div>
+				<form onSubmit={handleSubmit}>
+					<label htmlFor='email' className='emailLabel'>
+						{' '}
+					</label>
+					<input
+						id='email'
+						onChange={handleChange}
+						value={formState.email}
+						placeholder='Email'
+						className='emailInput'
+					/>{' '}
+					<br />
+					<label htmlFor='password' className='passwordLabel'>
+						{' '}
+					</label>
+					<input
+						id='password'
+						onChange={handleChange}
+						value={formState.password}
+						placeholder='Password'
+						className='passwordInput'
+					/>
+					<button type='submit' className='signinSubmit'>
+						Submit
+					</button>
+				</form>
+				<div className='register'>
+				<div className='registrationTitle'>Not Signed up? Register Below:</div>
 				<br />
-				<label htmlFor='password' className='passwordLabel'>
-					Enter Password:{' '}
-				</label>
-				<input
-					id='password'
-					onChange={handleChange}
-					value={formState.password}
-					placeholder='Password'
-					className='passwordInput'
-				/>
-				<button type='submit' className='loginSubmit'>
-					Submit
-				</button>
-			</form>
-			<div className='registrationTitle'>Not Signed up? Register Below:</div>
-			<br />
-			<Link to='/signup' className='registration'>
-				Register
-			</Link>
+				<Link to='/signup' className='registration'>
+					Sign Up
+				</Link>
+				</div>
+			</div>
+			<div id='signIn-img'>
+				<img src={signInImg} alt='signInImg'/>
+			</div>
 		</div>
 	);
 };
