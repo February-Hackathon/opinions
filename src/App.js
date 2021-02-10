d8ddebbb647891ddd05a9161ab050f55cd8113e6
AshveerB <ashveerbhayroo@gmail.com>
2021-02-09 22:06:57 -0800

    add routes for client side routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { React, useState } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import Navigation from '../src/components/navigation/Navigation';
+import Home from '../src/components/home/Home';
+import SignUp from '../src/components/signup/SignUp';
+import SignIn from '../src/components/signin/SignIn';
+import Doctors from '../src/components/doctors/Doctors';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loggedIn, setLoggedIn] = useState(
+		localStorage.getItem('token') ? true : false
+	);
+	const handleLogout = () => {
+		localStorage.clear();
+		setLoggedIn(false);
+	};
+	return (
+		<div className='App'>
+			<Navigation
+				loggedIn={loggedIn}
+				setLoggedIn={setLoggedIn}
+				handleLogout={handleLogout}
+			/>
+			<main>
+				<Route path='/home' component={Home} />
+				<Route path='/' exact render={() => <Redirect to='/home' />} />
+				<Route path='/signup' component={SignUp} />
+				<Route
+					path='/signin'
+					exact
+					render={() => (
+						<SignIn loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+					)}
+				/>
+				<Route
+					path='/doctors'
+					exact
+					render={() => {
+						return <Doctors loggedIn={loggedIn} />;
+					}}
+				/>
+			</main>
+		</div>
+	);
 }
 
 export default App;
