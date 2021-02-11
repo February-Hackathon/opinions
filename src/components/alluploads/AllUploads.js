f7cecc343da899fb4bf7b0dc51d0e5d5d1ba51bc
Ryan McLaughlin <itsryanmclaughlin@gmail.com>
2021-02-11 17:02:02 -0500

    responsive all uploads page

--- src/components/alluploads/AllUploads.js
+++ src/components/alluploads/AllUploads.js
@@ -6,15 +6,17 @@ import FooterImg from '../fileupload/Screen Shot 2021-02-10 at 12.59 1.jpg'
 const AllUploads = () => {
     return (
 			<div>
-				<div className='allUploadsTitle'>Upload Your Medical History</div>
-				<div className='audbContainer'>
-					<div className='allUploadsDropBox'>
-						<img src={FileImg} alt='fileImg' className='fileImg1' />
-						<img src={FileImg} alt='fileImg' className='fileImg2' />
-						<img src={FileImg} alt='fileImg' className='fileImg3' />
+				<div className='body5'>
+					<div className='allUploadsTitle'>Upload Your Medical History</div>
+					<div className='audbContainer'>
+						<div className='allUploadsDropBox'>
+							<img src={FileImg} alt='fileImg' className='fileImg1' id='fileImg1'/>
+							<img src={FileImg} alt='fileImg' className='fileImg2' id='fileImg2' />
+							<img src={FileImg} alt='fileImg' className='fileImg3' id='fileImg3' />
+						</div>
 					</div>
+					<button className='allUploadsNextBtn'>Next</button>
 				</div>
-				<button className='allUploadsNextBtn'>Next</button>
 				<img src={FooterImg} alt='footerImg' className='allUploadsFooter' />
 			</div>
 		);
