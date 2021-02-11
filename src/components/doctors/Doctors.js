576e19295f654c6d3c5e7a48673947c92643c912
Ryan McLaughlin <itsryanmclaughlin@gmail.com>
2021-02-11 15:01:19 -0500

    styled for screens above 600px

--- src/components/doctors/Doctors.js
+++ src/components/doctors/Doctors.js
@@ -1,7 +1,75 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './doctor.css';
+import doc1 from './doc1.png';
+import doc2 from './doc2.png';
+import doc3 from './doc3.png';
+import doc4 from './doc4.png';
+import doc5 from './doc5.png';
+
+
 
 const Doctors = ({ loggedIn }) => {
-	return <div style={{ display: loggedIn ? 'block' : 'none' }}>Doctors</div>;
+	// return <div style={{ display: loggedIn ? 'block' : 'none' }}>Doctors</div>;
+	return  <div>
+				<div className='body3'>
+					<div className='doctorTitle'>
+						Choose Your Specialist
+					</div>
+					<div className='doctorImages'>
+						<div id='doc1'>
+							<img src={doc1} width='150px' height='150px' border-radius='30%' alt='doc1'/>
+							<p>DR. LENNART
+								<br/>
+								Joined: 2020
+								<br/>
+								Chief Resident at California 
+								<br/>
+								Pacific Medical Center
+							</p>
+						</div>
+						<div id='doc2'>
+							<img src={doc2} width='150px' height='150px' alt='doc2'/>
+							<p>DR. DEAN
+								<br/>
+								Joined: 2020
+								<br/>
+								Chief Resident at California 
+								<br/>
+								Pacific Medical Center
+							</p>
+						</div>
+						<div id='doc3'>
+							<img src={doc3} width='150px' height='150px' alt='doc3'/>
+							<p>DR. CHAU
+								<br/>
+								Joined: 2020
+								<br/>
+								Chief Resident at California 
+								<br/>
+								Pacific Medical Center
+							</p>
+						</div>
+						<div id='doc4'>
+							<img src={doc4} width='150px' height='150px' alt='doc4'/>
+							<p>DR. FELDMAN
+								<br/>
+								Joined: 2020
+								<br/>
+								Chief Resident at California 
+								<br/>
+								Pacific Medical Center
+							</p>
+						</div>
+						<button type='submit' className='doctorSubmit'>
+							Submit
+						</button>
+					</div>
+				</div>
+				<div id='doc5'>
+					<img src={doc5} alt='do5'/>
+				</div>
+			</div>;
 };
 
 export default Doctors;
